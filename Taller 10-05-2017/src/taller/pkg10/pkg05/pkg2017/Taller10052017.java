b6a609cc026d359129e3c8678ea6654ed38b2d18
jahurtadod <jahurtadod@outlook.com>
2017-05-10 10:23:12 -0500

    Add files via upload

---
+++ Taller 10-05-2017/src/taller/pkg10/pkg05/pkg2017/Taller10052017.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller.pkg10.pkg05.pkg2017;
+
+/**
+ *
+ * @author DELL
+ */
+public class Taller10052017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
